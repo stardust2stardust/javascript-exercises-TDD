b85e4b791a1374a55cda91dd5e533caa8af7e0c2
Jeni Hoyt <ziahrahl@gmail.com>
2022-04-12 18:52:27 -0400

    exercise 3 (reverse string) completed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,15 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+   let letters = [];
+   let newString = "";
+   for (let char of string) {
+       letters.push(char);
+   } 
+   for (let i = letters.length - 1; i >= 0; i--){
+       newString += letters[i];
+   } 
+   return newString
 };
 
 // Do not edit below this line
 module.exports = reverseString;
+
