16358ae7caf42d6acf48ac892714e4fcabce0673
Jeni Hoyt <ziahrahl@gmail.com>
2022-04-13 11:00:56 -0400

    exercise 5 (sum all) completed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
-
+const sumAll = function(int1, int2) {
+    let finalSum = 0;
+    if (int1 < 0 || int2 < 0) {
+        return 'ERROR';
+    }
+    if (!Number.isInteger(int1) || !Number.isInteger(int2)) {
+        return 'ERROR';
+    }
+    if (int1 < int2) {
+        for (let i = int1; i <= int2; i++) {
+            finalSum += i;
+        } 
+    } else if (int2 < int1) {
+        for (let i = int2; i <= int1; i++) {
+            finalSum += i;
+        }
+    }
+   
+    return finalSum;
 };
 
 // Do not edit below this line
