bd35be70696eeb13536df906ca4336dd7ab442b7
Jeni Hoyt <ziahrahl@gmail.com>
2022-04-13 12:04:29 -0400

    exercise 7 (temp conversion) completed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(fTemp) {
+    let celsius = (fTemp - 32) * (5 / 9);
+    return Math.round(celsius * 10) / 10;
 };
 
-const ctof = function() {
-
+const ctof = function(cTemp) {
+    let fahren = (cTemp * (9 / 5)) + 32;
+    return Math.round(fahren * 10) / 10;
 };
 
 // Do not edit below this line
