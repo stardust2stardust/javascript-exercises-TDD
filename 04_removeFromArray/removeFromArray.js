49397be0f88110bc817a8dcdde59eaca30f972c2
Jeni Hoyt <ziahrahl@gmail.com>
2022-04-13 10:31:13 -0400

    exercise 4 (remove from array) completed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(listOfThings, ...optArgs) {
+    
+    for (let arg of optArgs) {
+        for (let i = 0; i < listOfThings.length; i++){
+            if (arg === listOfThings[i]) {
+                listOfThings.splice(i,1);
+            } 
+        }
+        
+    } return listOfThings;
 };
 
 // Do not edit below this line
