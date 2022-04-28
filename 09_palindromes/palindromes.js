77234b301062ead09d4e79a3bfde0ea32308f670
Jeni Hoyt <ziahrahl@gmail.com>
2022-04-28 14:35:53 -0400

    exercise 9 (palindromes) completed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,22 @@
-const palindromes = function () {
+const palindromes = function (phrase) {
+    const altPhrase = phrase.toLowerCase().replace(/\W/g, '')
+    const convertToArray = altPhrase.split('')
+    const reverseArray = convertToArray.reverse()
+    const joinLetters = reverseArray.join('')
+    console.log(joinLetters)
+    if (joinLetters == altPhrase) {
+        return true
+    } else {
+        return false
+    }
 
 };
 
+// const single = "this is not a palindrome!!"
+// console.log(palindromes(single))
+
+// const testPhrase = "I wwant % { kla ! . to return just letters"
+// const stripped = testPhrase.replace(/\W/g, '')
+// console.log(stripped)
 // Do not edit below this line
 module.exports = palindromes;
