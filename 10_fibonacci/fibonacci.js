c803b616e6ce67ca334fbee80dd346e80321f3b5
Jeni Hoyt <ziahrahl@gmail.com>
2022-04-28 15:18:19 -0400

    exercise 10 (fibonacci) completed

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,25 @@
-const fibonacci = function() {
+const fibonacci = function (n) {
+    if (typeof n === "string") {
+        n = parseInt(n);
+    }
+
+    if (n < 0) {
+        return "OOPS"
+    }
+    else if (n === 0) {
+        return 0
+    }
+    else if (n === 1) {
+        return 1
+    }
+    else if (n > 1) {
+        return fibonacci(n - 2) + fibonacci(n - 1)
+    }
 
 };
+
+const fib = fibonacci("1")
+console.log(fib)
 
 // Do not edit below this line
 module.exports = fibonacci;
