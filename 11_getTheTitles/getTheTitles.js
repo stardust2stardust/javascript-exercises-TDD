3cac5a685731e456dd944bd44b4d32cda6c0374b
Jeni Hoyt <ziahrahl@gmail.com>
2022-04-28 15:45:27 -0400

    exercise 11 (get Titles) completed

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,23 @@
-const getTheTitles = function() {
-
+const getTheTitles = function (array) {
+    listOfTitles = []
+    array.forEach(element => listOfTitles.push(element.title)
+    );
+    return listOfTitles
 };
+
+// const books = [
+//     {
+//         title: 'Book',
+//         author: 'Name'
+//     },
+//     {
+//         title: 'Book2',
+//         author: 'Name2'
+//     }
+// ]
+
+// console.log(getTheTitles(books))
+
 
 // Do not edit below this line
 module.exports = getTheTitles;
