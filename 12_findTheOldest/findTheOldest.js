60ffd35da1899d24a46dd204fd6a6ba2f7529819
Jeni Hoyt <ziahrahl@gmail.com>
2022-04-29 13:59:23 -0400

    exercise 12 (find the oldest) completed

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,42 @@
-const findTheOldest = function() {
+const findTheOldest = function (array) {
+    let oldestSoFar = 0;
+    let oldestPerson = {}
+    const currentYear = (new Date()).getFullYear();
+    let age;
+    array.forEach(element => {
+        if (!(element.hasOwnProperty('yearOfDeath'))) {
+            age = currentYear - element.yearOfBirth;
+        } else {
+            age = element.yearOfDeath - element.yearOfBirth;
+        }
 
+        if (age > oldestSoFar) {
+            oldestSoFar = age;
+            oldestPerson = element;
+        }
+    });
+    return oldestPerson
 };
+
+// const people = [
+//     {
+//         name: "Carly",
+//         yearOfBirth: 1066,
+//     },
+//     {
+//         name: "Ray",
+//         yearOfBirth: 1962,
+//         yearOfDeath: 2011,
+//     },
+//     {
+//         name: "Jane",
+//         yearOfBirth: 1912,
+//         yearOfDeath: 1941,
+//     },
+// ]
+
+// const oldest = findTheOldest(people)
+// console.log(oldest)
 
 // Do not edit below this line
 module.exports = findTheOldest;
